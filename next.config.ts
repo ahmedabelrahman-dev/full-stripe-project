2f695177b01c16a67950bac470f7049dec0eee1a
ahmedabelrahman-dev <rman79340@gmail.com>
2026-01-05 23:01:26 +0400

    build new stripe app to purches courses

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'i3.ytimg.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
